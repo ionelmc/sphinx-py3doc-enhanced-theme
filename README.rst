d90aeb221082f9f6b2facc6aab3eec4e17a9bd36
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-03-23 13:57:31 +0200

    Upgrade project skel.

--- README.rst
+++ README.rst
@@ -2,16 +2,55 @@
 Enhanced theme based on py3 documentation's theme
 =================================================
 
-.. image:: http://img.shields.io/pypi/v/sphinx_py3doc_enhanced_theme.png
-    :alt: PYPI Package
-    :target: https://pypi.python.org/pypi/sphinx_py3doc_enhanced_theme
+| |version| |downloads| |wheel| |supported-versions| |supported-implementations|
 
-.. image:: http://img.shields.io/pypi/dm/sphinx_py3doc_enhanced_theme.png
-    :alt: PYPI Package
-    :target: https://pypi.python.org/pypi/sphinx_py3doc_enhanced_theme
+.. |docs| image:: https://readthedocs.org/projects/sphinx-py3doc-enhanced-theme/badge/?style=flat
+    :target: https://readthedocs.org/projects/sphinx-py3doc-enhanced-theme
+    :alt: Documentation Status
+
+.. |travis| image:: http://img.shields.io/travis/ionelmc/sphinx-py3doc-enhanced-theme/master.png?style=flat
+    :alt: Travis-CI Build Status
+    :target: https://travis-ci.org/ionelmc/sphinx-py3doc-enhanced-theme
+
+.. |appveyor| image:: https://ci.appveyor.com/api/projects/status/github/ionelmc/sphinx-py3doc-enhanced-theme?branch=master
+    :alt: AppVeyor Build Status
+    :target: https://ci.appveyor.com/project/ionelmc/sphinx-py3doc-enhanced-theme
+
+.. |coveralls| image:: http://img.shields.io/coveralls/ionelmc/sphinx-py3doc-enhanced-theme/master.png?style=flat
+    :alt: Coverage Status
+    :target: https://coveralls.io/r/ionelmc/sphinx-py3doc-enhanced-theme
+
+.. |landscape| image:: https://landscape.io/github/ionelmc/sphinx-py3doc-enhanced-theme/master/landscape.svg?style=flat
+    :target: https://landscape.io/github/ionelmc/sphinx-py3doc-enhanced-theme/master
+    :alt: Code Quality Status
+
+.. |version| image:: http://img.shields.io/pypi/v/sphinx-py3doc-enhanced-theme.png?style=flat
+    :alt: PyPI Package latest release
+    :target: https://pypi.python.org/pypi/sphinx-py3doc-enhanced-theme
+
+.. |downloads| image:: http://img.shields.io/pypi/dm/sphinx-py3doc-enhanced-theme.png?style=flat
+    :alt: PyPI Package monthly downloads
+    :target: https://pypi.python.org/pypi/sphinx-py3doc-enhanced-theme
+
+.. |wheel| image:: https://pypip.in/wheel/sphinx-py3doc-enhanced-theme/badge.png?style=flat
+    :alt: PyPI Wheel
+    :target: https://pypi.python.org/pypi/sphinx-py3doc-enhanced-theme
+
+.. |supported-versions| image:: https://pypip.in/py_versions/sphinx-py3doc-enhanced-theme/badge.png?style=flat
+    :alt: Supported versions
+    :target: https://pypi.python.org/pypi/sphinx-py3doc-enhanced-theme
+
+.. |supported-implementations| image:: https://pypip.in/implementation/sphinx-py3doc-enhanced-theme/badge.png?style=flat
+    :alt: Supported imlementations
+    :target: https://pypi.python.org/pypi/sphinx-py3doc-enhanced-theme
+
+.. |scrutinizer| image:: https://img.shields.io/scrutinizer/g/ionelmc/sphinx-py3doc-enhanced-theme/master.png?style=flat
+    :alt: Scrutinizer Status
+    :target: https://scrutinizer-ci.com/g/ionelmc/sphinx-py3doc-enhanced-theme/
 
 A theme based on the theme of https://docs.python.org/3/ with some responsive enhancements.
 
+* Free software: BSD license
 Installation
 ============
 
--- setup.py
+++ setup.py
@@ -1,63 +1,68 @@
-# -*- encoding: utf8 -*-
-import glob
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
 import io
+import os
 import re
+from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
+from os.path import relpath
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
 
-
 def read(*names, **kwargs):
     return io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
+        encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
+
 setup(
-    name="sphinx_py3doc_enhanced_theme",
-    version="1.2.0",
-    license="BSD",
-    description="A theme based on the theme of https://docs.python.org/3/",
-    long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
-    author="Ionel Cristian Mărieș",
-    author_email="contact@ionelmc.ro",
-    url="https://github.com/ionelmc/sphinx-py3doc-enhanced-theme",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
+    name='sphinx-py3doc-enhanced-theme',
+    version='1.2.0',
+    license='BSD',
+    description='A theme based on the theme of https://docs.python.org/3/ with some responsive enhancements.',
+    long_description='%s\n%s' % (read('README.rst'), re.sub(':obj:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    author_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/sphinx-py3doc-enhanced-theme',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Utilities",
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
     ],
     keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
+        # eg: 'rst': ['docutils>=0.11'],
     },
     entry_points={
-        "console_scripts": [
-            "sphinx_py3doc_enhanced_theme = sphinx_py3doc_enhanced_theme:main"
-        ]
-    }
-
+    },
 )
